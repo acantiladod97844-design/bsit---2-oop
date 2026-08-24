ed583d4ef14599b33fd3b21b3aed23a835486bd0
Doniel Eunson Ko <deko74415@liceo.edu.ph>
2026-08-24 10:08:56 +0800

    Midterm Lab Exam - ATM Machine (Surname, Firstname)

---
+++ src/SavingsAccountImpl.java
@@ -0,0 +1,5 @@
+public class SavingsAccountImpl extends SavingsAccount {
+    public SavingsAccountImpl(String accountNumber, String ownerName, double openingBalance, double interestRate) {
+        super(accountNumber, ownerName, openingBalance, interestRate);
+    }
+}
